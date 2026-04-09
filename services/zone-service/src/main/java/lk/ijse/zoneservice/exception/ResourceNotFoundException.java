424923381e2a65df711922808b2d462304b59778
dilini <dilinichamikasilva@gmail.com>
2026-04-09 22:42:51 +0530

    Update configuration files for zone-service and sensor-service

--- services/zone-service/src/main/java/lk/ijse/zoneservice/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package lk.ijse.zoneservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
